765d0f8da1e4181f49d9e2b14111397f8ea5cf15
adibrusydi <adibrusydif@gmail.com>
2019-08-05 02:03:31 +0700

    change-section-name

--- src/layouts/ticket.tsx
+++ src/layouts/ticket.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from '@emotion/styled'
 import { widths, colors } from '../styles/variables'
 
-const SpeakerSection = () => {
+const TicketSection = () => {
   return (
     <Root>
       <SpeakerHeading>Ticket</SpeakerHeading>
@@ -15,21 +15,6 @@ const SpeakerSection = () => {
     </Root>
   )
 }
-
-const SpeakerSubheading = styled('h2')`
-  margin-top: 0;
-  margin-bottom: 24px;
-  text-align: center;
-  font-weight: 300;
-
-  &:after {
-    display: inline-block;
-    margin-left: 8px;
-    color: ${colors.red01};
-    font-weight: 400;
-    content: '/';
-  }
-`
 
 const SpeakerHeading = styled('h1')`
   margin-top: 0;
@@ -73,38 +58,4 @@ const Root = styled('section')`
   width: 60%;
 `
 
-const Inner = styled('div')`
-  width: 100%;
-  max-width: ${widths.lg}px;
-  margin: 0 auto;
-  padding: 36px 24px;
-  background-color: ${colors.grey05};
-  border-top-right-radius: 16px;
-  border-bottom-left-radius: 16px;
-  border-top-left-radius: 48px;
-  border-bottom-right-radius: 48px;
-  overflow: hidden;
-`
-
-const SectionHeading = styled('h1')`
-  margin-top: 0;
-  margin-bottom: 24px;
-  text-align: center;
-  font-weight: 500;
-
-  &:after {
-    display: inline-block;
-    margin-left: 8px;
-    color: ${colors.red01};
-    font-weight: 700;
-    content: '/';
-  }
-`
-
-const SpeakerList = styled('div')`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-`
-
-export default SpeakerSection
+export default TicketSection

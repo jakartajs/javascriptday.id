5578a6bdee5d678b37818a53a3e8a9b56b05a3ec
adibrusydi <adibrusydif@gmail.com>
2019-08-13 12:47:41 +0700

    update-13-agustus

--- src/components/home/HomeSection.tsx
+++ src/components/home/HomeSection.tsx
@@ -84,10 +84,18 @@ const HomeSection: React.SFC<HomeSection> = ({ className }) => (
             <Link to="/about/">About</Link>
           </li>
           <li>
-            <a href="https://cfp-perkodi.herokuapp.com/events/jsday-2019" target="_blank" rel="noopener noreferrer">Call for Papers</a>
+            <a href="https://www.loket.com/event/ruby-confid-jsday-2019-wa1" target="_blank" rel="noopener noreferrer">
+              Get Tickets
+            </a>
           </li>
           <li>
-            <a href="https://docs.google.com/forms/d/e/1FAIpQLSdZGVphPcskVQL5J5TWmiovyV0wTPR1zWcCl3xxzXiUT2A3UA/viewform" target="_blank" rel="noopener noreferrer">Become Sponsor</a>
+            <a
+              href="https://docs.google.com/forms/d/e/1FAIpQLSdZGVphPcskVQL5J5TWmiovyV0wTPR1zWcCl3xxzXiUT2A3UA/viewform"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Become Sponsor
+            </a>
           </li>
         </HomepageLinks>
       </HomepageContent>
